b6937efe7233aed639374697a0c03cd55cd6718a
rohitj-lgtm <rohitjadhav6598@gmail.com>
2026-08-18 10:34:31 +0530

    Add: island-perimeter - submission-2

---
+++ Data Structures & Algorithms/island-perimeter/submission-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int perimeter = 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int[][] dirs = {{-1,0}, {1,0}, {0,-1}, {0,1}};   // up, down, left, right
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                if(grid[i][j] == 1){
+                    for (int[] d : dirs) {
+                        int nr = i + d[0], nc = j + d[1];
+                        if(nr < 0 || nc < 0 || nr >= rows || nc >= cols || grid[nr][nc] == 0) perimeter++;
+                    }
+                }
+            }
+        }
+
+        return perimeter;
+    }
+}
